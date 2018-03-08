0620de872a98facc8aaca3b4b68155b4fd6c2d12
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:42:35 +0530

    Create 4.java

---
+++ Beginners/set10/4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class ddf{
+     public static void main(String aa[]){
+    int ae,b,c;
+    Scanner s=new Scanner(System.in);
+    ae=s.nextInt();
+    b=s.nextInt();
+    for(int i=1;i<=ae;i++){
+    
+        if(b==i){
+            System.out.println(i+"\n");
+        }
+    }          
+  }
+}
