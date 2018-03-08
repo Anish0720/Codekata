daccbb8e806de6ec7431ea7b0f8210f83907e3c6
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:41:51 +0530

    Create 3.java

---
+++ Beginners/set10/3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class kelv{
+
+     public static void main(String aa[]){
+    int a,b,cc;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    
+    double n=a+273.5;
+    System.out.println(n+"\n");           
+   }
+}
