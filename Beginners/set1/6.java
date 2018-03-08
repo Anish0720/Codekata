e054aff4a683319dc652d7e2434775d37a3e3998
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-08 23:59:15 +0530

    Create 6.java

---
+++ Beginners/set1/6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class leap{
+      public static void main(String aa[]){
+            int a;
+            Scanner s=new Scanner(System.in);
+             a=s.nextInt();
+             if(a%4==0){
+                System.out.println("\n\tIT ISLEAP YEAR"); 
+             }
+             else{
+                 System.out.println("\n\tNOT LEAP YEAR"); 
+             }
+    }
+}
