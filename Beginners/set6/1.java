de66207bb921174838725eee4edffa1d7c36f121
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:03:14 +0530

    Create 1.java

---
+++ Beginners/set6/1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class sd12{
+     public static void main(String aa[]){
+     String a;
+     int pxx;
+     
+     Scanner s=new Scanner(System.in);
+     px=s.nextInt();
+     a=String.valueOf(pxx);
+     char b[]=a.toCharArray();
+     for(int i=0;i<b.length;i++){
+         System.out.printf("\n%c\t",b[i]);
+     }
+  }
+}
