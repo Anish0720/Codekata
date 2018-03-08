3f78e1632447d089b23d6c2b1038ec76c83e30d7
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:20:24 +0530

    Create 9.java

---
+++ Beginners/set7/9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class diff{
+     public static void main(String aa[]){
+     int a;
+     int bb;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     bb=s.nextInt();
+     int c=a-bb;
+     if(c%2==0){
+         System.out.println("Even\n");
+     }
+     
+ else{
+           System.out.println("Odd\n");
+      }
+      
+    }
+}
