7cf9280948c228bf6604e0106943bb346d233f79
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:00:49 +0530

    Create 9.java

---
+++ Beginners/set5/9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class summ{
+     public static void main(String aa[]){
+     int z,b,f;
+     Scanner s=new Scanner(System.in);
+     z=s.nextInt();
+     b=s.nextInt();
+     f=z+b;
+     System.out.println(f);
+     
+     System.out.println("\n");
+    }
+}
