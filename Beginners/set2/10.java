4b21048babd2e3798c0a8ddb43bf7de8ee7d13f8
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:00:43 +0530

    Create 10.java

---
+++ Beginners/set2/10.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class mul{
+      public static void main(String aa[]){
+           int a,p,q;
+           int f;
+           int sum=0;
+           Scanner s=new Scanner(System.in);
+           a = s.nextInt();
+           for(int i=a;i<=a*a;i+=a){
+               System.out.printf("%d\n",i);
+           }     
+}
+
+}
