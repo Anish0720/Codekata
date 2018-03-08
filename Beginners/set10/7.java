89dcd6f20636fb3b0e2df75451154c10bd8468dd
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:44:50 +0530

    Create 7.java

---
+++ Beginners/set10/7.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rev11 {
+     public static void main(String aa[]){
+    int as,b,c;
+    Scanner s=new Scanner(System.in);
+   a=ss.nextInt();
+   String n=String.valueOf(as);
+   String z=new StringBuffer(n).reverse().toString();
+   
+   
+   System.out.println(z+"\n");          
+   }
+}
