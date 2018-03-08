68d7a2ce2aa3a445f0c004f760443707df30e0df
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:25:30 +0530

    Create 4.java

---
+++ Beginners/set8/4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class round{
+     public static void main(String aa[]){
+        float ds;
+        Scanner s=new Scanner(System.in);
+        ds=s.nextFloat();
+      int v=  Math.round(ds);
+      System.out.println(v);
+      
+      System.out.println("\n");
+    }
+}
