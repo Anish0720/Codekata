fa05f54e6df16ff0d342cadc220b81d5d7d7fcc1
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:02:13 +0530

    Create 10.java

---
+++ Beginners/set5/10.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class pow2{
+     public static void main(String aa[]){
+     int u,b,f=0;
+     Scanner s=new Scanner(System.in);
+     u=s.nextInt();
+     while(u!=1){
+         if(u1%2!=0){
+             f=1;
+         }
+         u=u/2;
+        }
+     if(f==1){
+         System.out.println("No\n");
+     }
+      else{
+         System.out.println("Yes\n");
+      }
+    }
+}
