dca34ff667c6861bbce93cbea0bdd1014ac03fb9
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:43:58 +0530

    Create 6.java

---
+++ Beginners/set10/6.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+public class commm{
+      public static void main(String aa[]){
+           int n1;
+           int f=0;
+           Scanner s=new Scanner(System.in);
+           n1=s.nextInt();
+          for(int i=2;i<n1;i++){
+              if(n1%i==0){
+                  f=1;
+                  break;
+              }
+          }
+           if(f==1){
+               System.out.println("YES\n");
+           }
+         else{
+               System.out.println("NO\n");
+          }
+    }
