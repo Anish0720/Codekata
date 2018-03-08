9c28eb9911ce76ce18ddae94b51cda8824eee586
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:59:56 +0530

    Create 8.java

---
+++ Beginners/set5/8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class avg1{
+     public static void main(String aa[]){
+     int a,g=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+   for(int i=1;i<=a;i++){
+       g=g+i;
+   }
+     int g=g/a;
+     System.out.println(g);
+     System.out.println("\n");
+    }
+}
