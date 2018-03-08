d24bbec422cad27bd32d3cc5ee5d5e778d657dd0
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:12:57 +0530

    Create 1.java

---
+++ Beginners/set7/1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class charf{
+     public static void main(String aa[]){
+     String d1;
+     int n;
+     Scanner s=new Scanner(System.in);
+     d1=s.next();
+     n=s.nextInt();
+     char a[]=d1.toCharArray();
+     for(int i=0;i<n;i++){
+        System.out.printf("%c\n",a[i]);
+        
+        
+     }
+     
+}
+}
