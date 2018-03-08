567b4d56a72ee98fab7368f0e9d404d77a6f7346
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:18:18 +0530

    Create 7.java

---
+++ Beginners/set7/7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class mull{
+     public static void main(String aa[]){
+     int aq;
+     int n=10;
+     Scanner s=new Scanner(System.in);
+     aq=s.nextInt();
+     for(int i=10;i<10000;i+=10){
+         if(aq<i){
+             System.out.println(aq);
+         }
+     System.out.println("\n");
+     }
+    }
+}
