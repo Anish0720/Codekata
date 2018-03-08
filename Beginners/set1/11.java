ca5bfe7ee0f9de815db7cae0bfef3092d92b6ad1
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 00:09:32 +0530

    Create 11.java

---
+++ Beginners/set1/11.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class len{
+      public static void main(String aa[]){
+            int b;
+            Scanner s=new Scanner(System.in);
+            b=s.nextInt();
+            String j=String.valueOf(b);
+            int li=j.length();
+            System.out.println(li); 
+    }
+      
+      
+}
