6dc08cf7e47ea89aad2f4c652c945bf43e9df7ed
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:24:44 +0530

    Create 3.java

---
+++ Beginners/set8/3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class bet {
+     public static void main(String aa[]){
+         int a;
+         int ff=0;
+         int b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    for(int i=b;i<=c;i++){
+        if(i==a){
+            f=1;
+            break;
+        }
+    }
+    
+    
+   if(ff==1){
+       System.out.println("Yes\n");
+   }
+ else{
+        System.out.println("NO\n");
+      }
+ }
+}
