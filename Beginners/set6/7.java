1c186d1e089d7d95c62e3ba0973093c671a0c801
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:08:33 +0530

    Create 7.java

---
+++ Beginners/set6/7.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+public class s1{
+     public static void main(String aa[]){
+     int a;
+     int b;
+     int t;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     t=a;
+     a=b;
+     b=t;
+     System.out.printf("%d %d\n",a,b);
+    }
+    
+}
