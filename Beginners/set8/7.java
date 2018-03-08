d5e961042e0346d978058e87d820368dce725880
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:28:43 +0530

    Create 7.java

---
+++ Beginners/set8/7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class f{
+     public static void main(String aa[]){
+       int b1;
+        Scanner s=new Scanner(System.in);
+        b1=s.nextInt();
+        for(int i=1;i<=b1;i++){
+            if(b1%i==0){
+            
+                System.out.print(i+"\n");
+             }
+        }
+    }
+}
