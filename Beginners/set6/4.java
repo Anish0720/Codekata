b0626e27eeeb492f7ab9d2f1de885571985f6c6f
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:05:59 +0530

    Create 4.java

---
+++ Beginners/set6/4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class neaev{
+     public static void main(String aa[]){
+     int n4;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n4=s.nextInt();
+     if(n4%2!=0){
+     
+         System.out.print(n4-1);
+     }
+     System.out.print("\n");
+}
+}
