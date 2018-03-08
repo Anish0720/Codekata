c2944d85ccc963073f210d039ab86a0cd5a8b5f4
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:10:39 +0530

    Create 9.java

---
+++ Beginners/set6/9.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+public class mm{
+      public static void main(String aa[]){
+       int nn;
+       int count=0;
+       int c;
+       ArrayList<Integer>a=new ArrayList<Integer>();
+       Scanner s=new Scanner(System.in);
+       n=s.nextInt();
+       for(int i=0;i<nn;i++){
+           c=s.nextInt();
+           a.add(c);
+       }
+       System.out.println(Collections.max(a));
+       
+       System.out.println("\t\n");
+}
+
+}
