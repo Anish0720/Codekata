c14554ce68ea1c703ab3e3fd4ea85773027c8ff6
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:43:16 +0530

    Create 5.java

---
+++ Beginners/set10/5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class call1{
+     public static void main(String aa[]){
+    int av,b,c;
+    Scanner s=new Scanner(System.in);
+    av=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    
+    int p=(av*b*c)/100;
+    System.out.println(p+"\n");          
+   }
+}
