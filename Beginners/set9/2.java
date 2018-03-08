e4ea0078056e5809bc6838f0358c582e878c5004
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:32:52 +0530

    Create 2.java

---
+++ Beginners/set9/2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class fuy{
+     public static void main(String aa[]){
+       Scanner s=new Scanner(System.in);
+       float a1=s.nextFloat();
+       float b=s.nextFloat();
+       float c=a1*b;
+       
+       System.out.printf("%f\n",c);
+    }
+}
