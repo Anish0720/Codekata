0b05c1f3f2561ef3d04b90e34369093e2d9cc2a3
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:28:48 +0530

    Create 7.java

---
+++ Beginners/set3/7.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class num{
+      public static void main(String aa[]){
+       char y;
+       Scanner s=new Scanner(System.in);
+       y=s.next().charAt(0);
+       if(y>='0'&&y<='9'){
+           System.out.println("\n\tYes");
+       }
+      else{
+           System.out.println("\n\tNo");
+      }
+}
+}
