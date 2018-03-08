446694609072b9aa41f92e24b421ff17d6e838ea
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:41:17 +0530

    Create 2.java

---
+++ Beginners/set10/2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class summ1{
+      public static void main(String aa[]){
+            int n1;
+            int a;
+            int sum=0;
+            Scanner s=new Scanner(System.in);
+            a=s.nextInt();
+            n1=s.nextInt();
+            int b[]=new int[n];
+            for(int i=0;i<n1;i++){
+                b[i]=s.nextInt();
+            }
+            for(int i=0;i<=a;i++){
+             sum=sum+b[i];
+             
+            }
+             System.out.println(sum+\n);
+    }
+}
