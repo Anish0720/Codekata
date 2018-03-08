96b1ea2fd45d3b6cf8759c26c68265e72048728e
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:47:24 +0530

    Create 7.java

---
+++ Beginners/set4/7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class swap{
+     public static void main(String aa[]){
+     int a1;
+     int b1;
+     int t1;
+     Scanner s=new Scanner(System.in);
+     a1=s.nextInt();
+     b1=s.nextInt();
+     t1=a1;
+     a1=b1;
+     b1=t1;
+     System.out.printf("%d %d\n",a1,b1);
+    }
+    
+    
+}
