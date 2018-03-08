15d89ecafed90e9b287845efd5bfd9ceb0f05c18
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:51:57 +0530

    Create 1.java

---
+++ Beginners/set5/1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class hell{
+     public static void main(String aa[]){
+     String a1;
+     int k;
+     Scanner s=new Scanner(System.in);
+     k=s.nextInt();
+     a1=s.next();
+     for(int i=1;i<=k;i++){
+        
+        System.out.println(a1);
+       
+       System.out.println("\n");
+     }
+    }
+}
