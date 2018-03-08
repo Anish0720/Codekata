0334e90a30ef3529ae07b8e2fe9a4c6afd74c02f
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:50:37 +0530

    Create 4.java

---
+++ Beginners/set11/4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class doupl{
+    public static void main(String aa[]){
+    double a,b1,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextDouble();
+    b1=s.nextDouble();
+    c=Math.pow(a, b1);
+    
+    System.out.print(c+"\n");     
+   }
+}
