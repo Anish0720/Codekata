f22356cee552a86749898673d6bedd591c479154
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:55:54 +0530

    Create 1.java

---
+++ Beginners/set12/1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class caal{
+    public static void main(String aa[]){
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    
+    String b=String.valueOf(a);
+    System.out.println(b.length()+"\n\n");          
+    }
+}
