b0a43511343be137f875b349e9d58d2e473cec65
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:58:04 +0530

    Create 6.java

---
+++ Beginners/set5/6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class nextn {
+     public static void main(String aa[]){
+     int aa,f=0;
+     Scanner s=new Scanner(System.in);
+     aa=s.nextInt();
+    aa=aa+1;
+    System.out.print(aa);
+    
+    System.out.print("\n");
+    }
+}
