653b84fd214b5a77a650be1f3aed57b82917987c
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-08 23:45:26 +0530

    Create 2.java

---
+++ Beginners/set1/2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Oddeve{
+      public static void main(String aa[]){
+            int n;
+            Scanner s=new Scanner(System.in);
+            n=s.nextInt();
+            if(n%2==0){
+                System.out.println("Even number it is!");
+            }
+         else{
+                System.out.println("Odd is rhe number");
+           }      
+      }
