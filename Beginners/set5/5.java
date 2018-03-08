dadb8cd6bc73276b0fb258ef299a64ffd2e61638
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:57:06 +0530

    Create 5.java

---
+++ Beginners/set5/5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class len1 {
+     public static void main(String aa[]){
+     int a,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     String c=String.valueOf(a);
+     int l1=c.length();
+     System.out.println(l1);
+     System.out.println("\n");
+     
+    }
+}
