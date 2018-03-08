3f642b5431d292ee3eccb8f90e2ab903f749ba97
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 01:24:10 +0530

    Create 4.java

---
+++ Beginners/set3/4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+public class sort{
+      public static void main(String aa[]){
+       int xx;
+       int count=0;
+       int c;
+       Scanner s=new Scanner(System.in);
+       x=s.nextInt();
+       int a[]=new int[n];
+       for(int i=0;i<x;i++){
+           a[i]=s.nextInt();
+       }
+      Arrays.sort(a);
+      for(int i=0;i<x;i++){
+          System.out.printf("%d \n",a[i]);
+      }
+}
+}
