efb364942fba703b9f2d3987e412c42376e9dd03
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:14:24 +0530

    Create 3.java

---
+++ Beginners/set7/3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class word{
+     public static void main(String aa[]){
+     String dd;
+     int n=0;;
+     Scanner s=new Scanner(System.in);
+     dd=s.nextLine();
+     String a[]=dd.split("\\s+");
+     int l=a.length;
+     
+     
+     System.out.println(l);
+     System.out.println("\n");
+}
