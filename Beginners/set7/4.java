87362c6534fbfb30ab7712b81026fac8c784ae1b
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2018-03-09 02:15:26 +0530

    Create 4.java

---
+++ Beginners/set7/4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class oddee{
+     public static void main(String aa[]){
+     int z,b;
+     int n;
+     Scanner s=new Scanner(System.in);
+    z=s.nextInt();
+    b=s.nextInt();
+    n=z+b;
+    if(n%2==0){
+        System.out.println("Even\n");
+    }
+    
+ else{
+        System.out.println("Odd\n");
+ }
+}
+    }
